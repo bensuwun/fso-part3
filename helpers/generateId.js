ed0c9e0dc554621b2e559cd8b65a73f515b68f76
jtp <benedict_sun@dlsu.edu.ph>
2023-12-29 14:59:29 +0800

    Added 3.1-3.6

---
+++ helpers/generateId.js
@@ -0,0 +1,7 @@
+const generateId = (persons) => {
+    return Math.floor(Math.random() * 999999999)
+}
+
+module.exports = {
+    generateId
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+const moment = require('moment');
+const express = require('express');
+const { generateId } = require('./helpers/generateId.js');
+const app = express();
+app.use(express.json());
+
+let persons = require('./data/persons.json');
+
+// Retrieve all persons
+app.get(`/api/persons`, (request, response) => {
+    response.json(persons);
+})
+
+// Retrieve single person
+app.get(`/api/persons/:id`, (request, response) => {
+    const id = parseInt(request.params.id);
+    const person = persons.find(person => person.id === id);
+    if (person)
+        response.json(person);
+    else{
+        response.status(404).send({ error: "Missing Person" });
+    }
+})
+
+// Delete phonebook entry
+app.delete('/api/persons/:id', (request, response) => {
+    const id = parseInt(request.params.id);
+    persons = persons.filter(person => person.id !== id);
+    response.status(204).end();
+});
+
+// Add new phonebook entry
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    if (!body.name || !body.number)
+        return response.status(400).send( { error: 'New entry must contain both name and number.' } );
+    else if (persons.find(person => person.name.toLowerCase() === body.name.toLowerCase()))
+        return response.status(400).send( { error: 'Name must be unique.' });
+
+    const person = {
+        id: generateId(persons),
+        name: body.name,
+        number: body.number
+    };
+    persons.push(person);
+
+    response.json(person);
+});
+
+// Info
+app.get(`/info`, (request, response) => {
+    const nPeople = persons.length;
+    const currentTime = moment().format('LLLL');
+    const result = `
+        <p>Phonebook has info for ${nPeople} people</p>
+        <p>${currentTime}</p>
+    `;
+    response.send(result);
+});  
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
