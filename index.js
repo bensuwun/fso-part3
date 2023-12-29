a5963c00f9b2d4f13b8b86bf48712965ae2af76b
jtp <benedict_sun@dlsu.edu.ph>
2023-12-29 19:57:29 +0800

    Added 3.9-3.11

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ morgan.token('body', (request, response) => {
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());  
+app.use(express.static('dist'));
 
 // Retrieve all persons
 app.get(`/api/persons`, (request, response) => {
